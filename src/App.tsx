8d59ea82cce9070c09889dfed8face1cc98203f6
chandrabumi26 <chandra.bumi@gmail.com>
2026-04-09 22:25:20 +0700

    protect route

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,21 @@ import Login from './pages/Login';
 import ProductList from './pages/ProductList';
 import ProductDetail from './pages/ProductDetail';
 import ProductForm from './pages/ProductForm';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Home />} />
-        <Route path="products" element={<ProductList />} />
-        <Route path="products/:id" element={<ProductDetail />} />
-        <Route path="products/add" element={<ProductForm />} />
-        <Route path="products/edit/:id" element={<ProductForm />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<ProductList />} />
+          <Route path="products/:id" element={<ProductDetail />} />
+          <Route path="products/add" element={<ProductForm />} />
+          <Route path="products/edit/:id" element={<ProductForm />} />
+        </Route>
       </Route>
 
       <Route path="*" element={<Navigate to="/" replace />} />
